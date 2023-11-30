329d3bf6b63cc49a68194ce0a1a6eebc6b053f30
Fawaz Bilal <bilal.fawaz@student.ehb.be>
2023-11-30 11:34:15 +0100

    I've created a Knex.js migration file to establish a users table in my database.
    I've created a Knex.js migration file to set up a users table in my database. This table includes columns for id, firstname, lastname, and email (unique and required). The migration file facilitates easy updates and rollbacks of my database during the development of my application.

--- images/api/src/db/migrations/20231110114320_initial.js
+++ images/api/src/db/migrations/20231110114320_initial.js
@@ -3,13 +3,26 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    return knex.schema.createTable('users', function(table) {
+      table.increments('id').primary();
+      table.string('firstname').notNullable();
+      table.string('lastname').notNullable();
+      table.string('email').notNullable().unique();
+      // Add more columns as needed
+      // table.string('other_column').notNullable();
+      // ...
   
-};
+      // Timestamps (optional)
+      table.timestamps(true, true);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+  };
+  
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
