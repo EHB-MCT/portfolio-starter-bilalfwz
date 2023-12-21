1e87a4c2a46f58a919e4c105dcfe47389ba9ae94
Fawaz Bilal <bilal.fawaz@student.ehb.be>
2023-12-21 22:18:56 +0100

    feat(database): add migration for creating 'students' table

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const knex = require("knex");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: '.env'});
+dotenv.config({ path: '.env' });
 
 const app = express();
 
 const knexfile = require("./db/knexfile");
 const db = knex(knexfile.development);
+
+app.use(express.json());
 
 // console.log(db.raw("SELECT 1+1"))
 app.get("/", (request,response ) =>{
@@ -15,39 +17,37 @@ app.get("/", (request,response ) =>{
 })
 
 app.post('/api/students', async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      error: "Request body is missing or empty",
+    });
+  }
 
-    if (!req.body) {
-        return res.status(400).send({
-            error: "Request body is missing or empty",
-        });
-    }
-  
-    const { id, first_name, last_name, age, email } = req.body;
-    try {
-        await db('students').insert({
-            id,
-            first_name,
-            last_name,
-            age,
-            email,
-        });
-        res.status(201).send({
-            message: 'Student created successfully',
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({
-            error: "Something went wrong",
-            value: error,
-        });
-    }
-  });
+  const { id, first_name, last_name, age, email } = req.body;
+  try {
+    await db('students').insert({
+      id,
+      first_name,
+      last_name,
+      age,
+      email,
+    });
+    res.status(201).send({
+      message: 'User created successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error: "Something went wrong",
+      value: error,
+    });
+  }
+});
 
 app.listen(3000, (err) => {
-    if(!err) {
-        console.log("running on port " + 3000);
-    }
-    else {
-        console.error(err)
-    }
-})
+  if (!err) {
+    console.log("Running on port " + 3000);
+  } else {
+    console.error(err);
+  }
+});
